3fcb84065078a3d182c41f2ab5d0acbdbab48635
Priyanshu Gautam <priyanshugautam.vns@gmail.com>
2026-08-05 21:18:27 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (59.50%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
-    int cal(vector<vector<int>>& triangle,int r,int c,int m,int n,vector<vector<int>>&dp)
-    {
-        if(dp[r][c]!=-2)
-        {
-            return dp[r][c];
-        }
-        if(r==m-1)
-        {
-            return dp[r][c]=triangle[r][c];
-        }
-        return dp[r][c]=triangle[r][c]+min(cal(triangle,r+1,c,m,n,dp),cal(triangle,r+1,c+1,m,n,dp));
-    }
+    // int cal(vector<vector<int>>& triangle,int r,int c,int m,int n,vector<vector<int>>&dp)
+    // {
+    //     if(dp[r][c]!=-2)
+    //     {
+    //         return dp[r][c];
+    //     }
+    //     if(r==m-1)
+    //     {
+    //         return dp[r][c]=triangle[r][c];
+    //     }
+    //     return dp[r][c]=triangle[r][c]+min(cal(triangle,r+1,c,m,n,dp),cal(triangle,r+1,c+1,m,n,dp));
+    // }
     int minimumTotal(vector<vector<int>>& triangle) {
-        
         int m=triangle.size();
         int n=triangle[m-1].size();
         vector<vector<int>>dp(m,vector<int>(n,-2));
-        return cal(triangle,0,0,m,n,dp);
+        for(int i=m-1;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                if(i==m-1)
+                {
+                    dp[i][j]=triangle[i][j];
+                }
+                else{dp[i][j]=triangle[i][j]+min(dp[i+1][j],dp[i+1][j+1]);}
+            }
+        }
+        return dp[0][0];
     }
 };
