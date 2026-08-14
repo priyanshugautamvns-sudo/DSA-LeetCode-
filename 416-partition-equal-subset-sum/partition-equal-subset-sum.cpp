181700af3da9dcf0f45aafb21ebce5bde21e8fce
Priyanshu Gautam <priyanshugautam.vns@gmail.com>
2026-08-14 15:19:56 +0530

    Time: 123 ms (62.66%) | Memory: 111.4 MB (13.68%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    bool dpp(vector<int>&nums,int target,vector<vector<int>>&dp,int i,int n)
-    {
-        if(target==0)
-        {
-            return true;
-        }
-        else if(i>=n)
-        {
-            return false;
-        }
-        if(dp[i][target]!=-1)
-        {
-            return dp[i][target];
-        }
-        bool c1=false;
-        bool c2=false;
-        if(i<n && nums[i]<=target)
-        {
-            c1=dpp(nums,target-nums[i],dp,i+1,n);
-            c2=dpp(nums,target,dp,i+1,n);
-        }
-        else if(nums[i]>target)
-        {
-            c2=dpp(nums,target,dp,i+1,n);
-        }
-        return dp[i][target]=c1||c2;
-    }
+    // bool dpp(vector<int>&nums,int target,vector<vector<int>>&dp,int i,int n)
+    // {
+    //     if(target==0)
+    //     {
+    //         return true;
+    //     }
+    //     else if(i>=n)
+    //     {
+    //         return false;
+    //     }
+    //     if(dp[i][target]!=-1)
+    //     {
+    //         return dp[i][target];
+    //     }
+    //     bool c1=false;
+    //     bool c2=false;
+    //     if(i<n && nums[i]<=target)
+    //     {
+    //         c1=dpp(nums,target-nums[i],dp,i+1,n);
+    //         c2=dpp(nums,target,dp,i+1,n);
+    //     }
+    //     else if(nums[i]>target)
+    //     {
+    //         c2=dpp(nums,target,dp,i+1,n);
+    //     }
+    //     return dp[i][target]=c1||c2;
+    // }
     bool canPartition(vector<int>& nums) {
         int sum=0;
         int target;
@@ -37,7 +37,24 @@ public:
         }
         if(sum%2!=0) return false;
         else target=sum/2;
-        vector<vector<int>>dp(n,vector<int>(target+1,-1));
-        return dpp(nums,target,dp,0,n);
+        vector<vector<int>>dp(n+1,vector<int>(target+1,false));
+        for(int i=0;i<dp.size();i++)
+        {
+            dp[i][0]=true;
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=1;j<=target;j++)
+            {
+                bool c1=false;
+                bool c2=dp[i+1][j];
+                if(nums[i]<=j)
+                {
+                    c1=dp[i+1][j-nums[i]];
+                }
+                dp[i][j]=c1||c2;
+            }
+        }
+        return dp[0][target];
     }
 };
