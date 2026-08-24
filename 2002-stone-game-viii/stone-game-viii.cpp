da126a851bdd13b7970219a0e54fba46a26e9e18
Priyanshu Gautam <priyanshugautam.vns@gmail.com>
2026-08-24 22:18:19 +0530

    Time: 100 ms (78.18%) | Memory: 101.4 MB (25.77%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.cpp
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -2,34 +2,21 @@ class Solution {
 public:
 
     int dpp(vector<int>& prefix, int i,
-            vector<int>& dp, int n)
+            vector<int>& dp,int n)
     {
-        if(i == n - 1)
-            return prefix[i];
-
-        if(dp[i] != -1)
-            return dp[i];
-        int v=dpp(prefix, i + 1, dp, n);
-        int take = prefix[i] - v;
-
-        int skip = v;
-
-        return dp[i] = max(take, skip);
+        if(i==n-1) return prefix[i];
+        if(dp[i]!=-1) return dp[i];
+        int skip=dpp(prefix,i+1,dp,n);
+        int take=prefix[i]-skip;
+        
+        return dp[i]=max(take,skip);
     }
-
     int stoneGameVIII(vector<int>& stones) {
-
-        int n = stones.size();
-
-        vector<int> prefix(n);
-
+        vector<int> prefix(stones.size());
         prefix[0] = stones[0];
-
-        for(int i = 1; i < n; i++)
+        for(int i = 1; i < stones.size(); i++)
             prefix[i] = prefix[i-1] + stones[i];
-
-        vector<int> dp(n, -1);
-
-        return dpp(prefix, 1, dp, n);
+        vector<int> dp(stones.size(), -1);
+        return dpp(prefix, 1, dp,stones.size());
     }
 };
