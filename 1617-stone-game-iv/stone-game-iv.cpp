f83b21af992db4bf9c104e5ed6f3353690803a9c
Priyanshu Gautam <priyanshugautam.vns@gmail.com>
2026-09-01 21:56:38 +0530

    Time: 71 ms (17.43%) | Memory: 11 MB (35.04%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.cpp
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
-    bool solve(int n, vector<int>& dp) {
-        if (n == 0) return false;
+    // bool solve(int n, vector<int>& dp) {
+    //     if (n == 0) return false;
 
-        if (dp[n] != -1)
-            return dp[n];
+    //     if (dp[n] != -1)
+    //         return dp[n];
 
-        for (int i = 1; i * i <= n; i++) {
-            if (!solve(n - i * i, dp)) {
-                return dp[n] = true;
-            }
-        }
+    //     for (int i = 1; i * i <= n; i++) {
+    //         if (!solve(n - i * i, dp)) {
+    //             return dp[n] = true;
+    //         }
+    //     }
 
-        return dp[n] = false;
-    }
+    //     return dp[n] = false;
+    // }
 
     bool winnerSquareGame(int n) {
         vector<int> dp(n + 1, -1);
-        return solve(n, dp);
+        dp[0]=false;
+        for(int j=1;j<=n;j++)
+        {
+            for (int i = 1; i * i <= j; i++) {
+                if (!dp[j-i*i]) {
+                    dp[j] = true;
+                }
+            }
+            if(dp[j]==-1)
+            {dp[j] = false;}
+        }
+        return dp[n];
     }
 };
