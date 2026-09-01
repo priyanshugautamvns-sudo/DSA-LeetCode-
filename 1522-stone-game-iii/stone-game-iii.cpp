f69d3190f0fb4d69f18ea50b3842b18063a23b8a
Priyanshu Gautam <priyanshugautam.vns@gmail.com>
2026-09-01 19:48:59 +0530

    Time: 7 ms (93.89%) | Memory: 139.9 MB (52.11%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -1,31 +1,51 @@
 class Solution {
 public:
-    int dpp(int i,vector<int>&dp,vector<int>& stoneValue,int n)
-    {
-        if(i==n) return 0;
-        if(dp[i]!=INT_MIN) return dp[i];
-        int sum=stoneValue[i];
-        int c1=sum-dpp(i+1,dp,stoneValue,n);
-        int c2=INT_MIN;
-        if(i+1<n)
-        {
-            sum+=stoneValue[i+1];
-            c2=sum-dpp(i+2,dp,stoneValue,n);
-        }
-        int c3=INT_MIN;
-        if(i+2<n)
-        {
-            sum+=stoneValue[i+2];
-            c3=sum-dpp(i+3,dp,stoneValue,n);
-        }
-        return dp[i]=max(c1,max(c2,c3));
-    }
+    // int dpp(int i,vector<int>&dp,vector<int>& stoneValue,int n)
+    // {
+    //     if(i==n) return 0;
+    //     if(dp[i]!=INT_MIN) return dp[i];
+    //     int sum=stoneValue[i];
+    //     int c1=sum-dpp(i+1,dp,stoneValue,n);
+    //     int c2=INT_MIN;
+    //     if(i+1<n)
+    //     {
+    //         sum+=stoneValue[i+1];
+    //         c2=sum-dpp(i+2,dp,stoneValue,n);
+    //     }
+    //     int c3=INT_MIN;
+    //     if(i+2<n)
+    //     {
+    //         sum+=stoneValue[i+2];
+    //         c3=sum-dpp(i+3,dp,stoneValue,n);
+    //     }
+    //     return dp[i]=max(c1,max(c2,c3));
+    // }
     string stoneGameIII(vector<int>& stoneValue) {
         int n=stoneValue.size();
-        vector<int>dp(n,INT_MIN);
-        int s=dpp(0,dp,stoneValue,n);
-        if(s==0) return "Tie";
-        else if(s>0) return "Alice";
+        vector<int>dp(3,INT_MIN);
+        dp[0]=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            int sum=stoneValue[i];
+            int c1=sum-dp[0];
+            int c2=INT_MIN;
+            if(dp[1]!=INT_MAX && i+1<n)
+            {
+                sum+=stoneValue[i+1];
+                c2=sum-dp[1];
+            }
+            int c3=INT_MIN;
+            if(dp[2]!=INT_MAX && i+2<n)
+            {
+                sum+=stoneValue[i+2];
+                c3=sum-dp[2];
+            }
+            dp[2]=dp[1];
+            dp[1]=dp[0];
+            dp[0]=max(c1,max(c2,c3));
+        }
+        if(dp[0]==0) return "Tie";
+        else if(dp[0]>0) return "Alice";
         else return "Bob";
     }
 };
