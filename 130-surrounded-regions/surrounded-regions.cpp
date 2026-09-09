75504bc4b31d208350522905842a36286677bc28
Priyanshu Gautam <priyanshugautam.vns@gmail.com>
2026-09-09 17:41:01 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (47.44%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
+    void dfs(vector<vector<char>>& board,vector<vector<int>>&vis,int i,int j,int n,int m)
+    {
+        vis[i][j]=1;
+        if(i-1>=0 && board[i-1][j]=='O' && vis[i-1][j]==0) dfs(board,vis,i-1,j,n,m);
+        if(i+1<=n-1 && board[i+1][j]=='O' && vis[i+1][j]==0) dfs(board,vis,i+1,j,n,m);
+        if(j+1<=m-1 && board[i][j+1]=='O' && vis[i][j+1]==0) dfs(board,vis,i,j+1,n,m);
+        if(j-1>=0 && board[i][j-1]=='O' && vis[i][j-1]==0) dfs(board,vis,i,j-1,n,m);
+    }
     void solve(vector<vector<char>>& board) {
         int n=board.size();
         int m=board[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
-        queue<pair<int,int>>q;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(board[i][j]=='O' && (i==0 || j==0 || i==n-1 || j==m-1)) 
+                if(board[i][j]=='O' && (i==0 || i==n-1 || j==0 || j==m-1))
                 {
-                    q.push({i,j});
-                    vis[i][j]=1;
-                }
-            }
-        }
-        vector<int>dx={1,-1,0,0};
-        vector<int>dy={0,0,1,-1};
-        while(!q.empty())
-        {
-            int x=q.front().first;
-            int y=q.front().second;
-            q.pop();
-            for(int i=0;i<4;i++)
-            {
-                int x1=x+dx[i];
-                int y1=y+dy[i];
-                if((x1>=0 && y1>=0) && (x1<n && y1<m))
-                {
-                    if(board[x][y]=='O' && vis[x1][y1]==0)
+                    if(vis[i][j]==0)
                     {
-                        vis[x1][y1]=1;
-                        q.push({x1,y1});
+                        dfs(board,vis,i,j,n,m);
                     }
                 }
             }
@@ -41,7 +29,7 @@ public:
         {
             for(int j=0;j<m;j++)
             {
-                if(vis[i][j]==0 && board[i][j]=='O') 
+                if(board[i][j]=='O' && vis[i][j]==0)
                 {
                     board[i][j]='X';
                 }
